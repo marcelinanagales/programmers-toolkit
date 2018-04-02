9ca78253a500d42e4b0c907b9d9f494b964d05d3
Steve <mln13@my.fsu.edu>
2018-04-02 15:14:26 -0400

    steve files

---
+++ example2.cpp
@@ -0,0 +1,23 @@
+#include <complex.h>
+#include <fftw3.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int N = 5;
+
+	fftw_complex *in, *out;
+	fftw_plan p;
+
+	in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+	out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+	p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+
+	fftw_execute(p); /*
+	repeat as needed
+	*/
+
+	fftw_destroy_plan(p);
+	fftw_free(in); fftw_free(out);
+}
